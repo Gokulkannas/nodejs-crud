6f24f5d2a569bb224edf1aa209e4fc2ee7830e31
Gokulkannas <100984277+Gokulkannas@users.noreply.github.com>
2023-07-03 12:32:03 +0530

    3/7/23  1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
 app.set("views", "./html");
 
 mongoose
